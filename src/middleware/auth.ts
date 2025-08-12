8efd689fea42d301eb918cad0830618ca03b57c5
TheLeopard17 <alessandrofallo8@gmail.com>
2025-08-12 06:11:40 +0200

    Push

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -14,14 +14,14 @@ export default function authenticateToken(
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   if (!token) {
-    res.sendStatus(401);
+    res.status(401).json({ message: 'Authorization token required' });
     return;
   }
 
   const secret = process.env.JWT_SECRET as string;
   jwt.verify(token, secret, (err, user) => {
     if (err) {
-      res.sendStatus(403);
+      res.status(403).json({ message: 'Invalid token' });
       return;
     }
     req.user = user;
